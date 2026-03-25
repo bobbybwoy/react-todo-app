83a6a9add588ed808517a0e251e0e606a2c4836b
bobbybwoy <68812513+bobbybwoy@users.noreply.github.com>
2026-03-25 15:44:26 +0000

    Implemented styling for GameItem component

--- src/components/GameItem/GameItem.jsx
+++ src/components/GameItem/GameItem.jsx
@@ -4,7 +4,7 @@ const GameItem = ({ game }) => {
 
     return (
         <article className="game-item">
-            <h3>{game.name}</h3>
+            <h3 className="game-name">{game.name}</h3>
             <div className="game-content">
                 <section className="game-description">{game.description}</section>
                 <section className="game-info">
